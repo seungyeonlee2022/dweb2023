fe59f521c29ad8a2140e4070dd88314617893ea4
seungyeonlee2022 <113084420+seungyeonlee2022@users.noreply.github.com>
2023-08-14 21:02:02 +0900

    Update gritvideo.js

--- custom_pages/js/gritvideo.js
+++ custom_pages/js/gritvideo.js
@@ -1,23 +1,20 @@
 let container1 = document.querySelectorAll("movieli_container")[0];
 let container2 = document.querySelectorAll("movieli_container")[1];
 let container3 = document.querySelectorAll("movieli_container")[2];
+let sammov = document.getElementsByClassName("sampleMovie");
+function f1(){
+    var movSrc = 'https://www.youtube.com/embed/'+ 'H14bBuluwB8';
+    sammov.src = movSrc;
+}
+function f2(){
+    var movSrc = 'https://www.youtube.com/embed/'+ '19iEEx-2hxk';
+    sammov.src= movSrc;
+}
+function f3(){
+    var movSrc = 'https://www.youtube.com/embed/'+ 'fjXZBr07tOo';
+    sammov.src= movSrc;
+}
 
 container1.addEventListener('click', f1);
 container2.addEventListener('click', f2);
 container3.addEventListener('click', f3);
-
-function f1(){
-    var movSrc = 'https://www.youtube.com/embed/'+ 'H14bBuluwB8';
-    let sammov = document.getElementsByClassName("sampleMovie");
-    sammov.src = movSrc;
-}
-function f2(){
-    let movSrc = 'https://www.youtube.com/embed/'+ '19iEEx-2hxk';
-    let sammov = document.getElementsByClassName("sampleMovie");
-    sammov.src= movSrc;
-}
-function f3(){
-    let movSrc = 'https://www.youtube.com/embed/'+ 'fjXZBr07tOo';
-    let sammov = document.getElementsByClassName("sampleMovie");
-    sammov.src= movSrc;
-}
