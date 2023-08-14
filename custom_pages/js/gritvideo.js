d6a39d93651d2343cddaec846006d805ea9aadc4
seungyeonlee2022 <113084420+seungyeonlee2022@users.noreply.github.com>
2023-08-14 17:41:59 +0900

    Update gritvideo.js

--- custom_pages/js/gritvideo.js
+++ custom_pages/js/gritvideo.js
@@ -1,4 +1,4 @@
-$(".YoutubeMovieList ul li a").click(function(){
+$(".YoutubeMovieList ul li button a").click(function(){
      
     var movSrc = 'https://www.youtube.com/embed/'+$(this).data('url')+'?autoplay=1';
     $("#sampleMovie").attr('src',movSrc);
