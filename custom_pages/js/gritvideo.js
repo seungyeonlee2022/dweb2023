af2bb7dac5329d8f558edd8e31b298d250788820
seungyeonlee2022 <113084420+seungyeonlee2022@users.noreply.github.com>
2023-08-14 21:40:36 +0900

    Update gritvideo.js

--- custom_pages/js/gritvideo.js
+++ custom_pages/js/gritvideo.js
@@ -1,5 +1,22 @@
-let sammov = document.querySelector(".sampleMovie");
-function changeMovie(i){
-    var movSrc = 'https://www.youtube.com/embed/'+ i;
-    sammov.src = movSrc;
-}
+const s = document.querySelector(".sampleMovie");
+
+ function f1() { 
+    s.src = 'https://www.youtube.com/embed/H14bBuluwB8';
+    s.load();
+    s.play();
+ }
+
+ function f2() { 
+    s.src = 'https://www.youtube.com/embed/19iEEx-2hxk';
+    s.load();
+    s.play();
+ }
+ function f3() { 
+    s.src = 'https://www.youtube.com/embed/fjXZBr07tOo';
+    s.load();
+    s.play();
+ }
+
+ document.getElementById("g1").onclick=f1;       
+ document.getElementById("g2").onclick=f2; 
+ document.getElementById("g3").onclick=f3; 
