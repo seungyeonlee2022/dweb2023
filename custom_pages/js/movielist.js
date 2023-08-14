b45f79b198581452bb1f8217ee95063140d5ff83
seungyeonlee2022 <113084420+seungyeonlee2022@users.noreply.github.com>
2023-08-14 15:58:02 +0900

    Add files via upload

---
+++ custom_pages/js/movielist.js
@@ -0,0 +1,8 @@
+$(".YoutubeMovieList ul li a").click(function(){
+     
+    var movSrc = 'https://www.youtube.com/embed/'+$(this).data('url')+'?autoplay=1';
+    $("#sampleMovie").attr('src',movSrc);
+ 
+    return false;
+});
+
