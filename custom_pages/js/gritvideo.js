236a7345cafdc742cde3689f83a47d4a200b9440
seungyeonlee2022 <113084420+seungyeonlee2022@users.noreply.github.com>
2023-08-14 18:42:29 +0900

    Update gritvideo.js

--- custom_pages/js/gritvideo.js
+++ custom_pages/js/gritvideo.js
@@ -2,20 +2,20 @@ let container1 = document.querySelectorAll("movieli_container")[0];
 let container2 = document.querySelectorAll("movieli_container")[1];
 let container3 = document.querySelectorAll("movieli_container")[2];
 
-container1.click(function(){
+container1.addEventListener('click', function(){
     var movSrc = 'https://www.youtube.com/embed/'+ 'H14bBuluwB8';
     let sammov = document.getElementsByClassName("sampleMovie");
     sammov.attr('src',movSrc);
     return false;
 });
-container2.click(function(){
+container2.addEventListener('click', function(){
     let movSrc = 'https://www.youtube.com/embed/'+ '19iEEx-2hxk';
     let sammov = document.getElementsByClassName("sampleMovie");
     sammov.src= movSrc;
     return false;
 });
 
-container3.click(function(){
+container3.addEventListener('click', function(){
     let movSrc = 'https://www.youtube.com/embed/'+ 'fjXZBr07tOo';
     let sammov = document.getElementsByClassName("sampleMovie");
     sammov.src= movSrc;
