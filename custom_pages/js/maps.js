f9f1bf12a565c19ee498ab9f23f7d276f83b3388
seungyeonlee2022 <113084420+seungyeonlee2022@users.noreply.github.com>
2023-08-17 21:39:31 +0900

    Update maps.js

--- custom_pages/js/maps.js
+++ custom_pages/js/maps.js
@@ -8,17 +8,17 @@ const button2 = document.getElementById('m2');
 const button3 = document.getElementById('m3');
 
 function see_map1(){
-  map1.classList.remove('map-hide');
+  map1.getAttribute('map-hide');
   map2.setAttribute('class','map-hide');
   map3.setAttribute('class','map-hide');
 }
 function see_map2(){
-  map2.classList.remove('map-hide');
+  map2.getAttribute('map-hide');
   map1.setAttribute('class','map-hide');
   map3.setAttribute('class','map-hide');
 }
 function see_map3(){
-  map3.classList.remove('map-hide');
+  map3.getAttribute('map-hide');
   map2.setAttribute('class','map-hide');
   map1.setAttribute('class','map-hide');
 }
